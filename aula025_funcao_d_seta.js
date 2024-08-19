422fec5039cb32ec5894a656345ba9e3e63b8ce2
Paulo-V-Developer34 <165535750+Paulo-V-Developer34@users.noreply.github.com>
2024-08-18 22:30:43 -0300

    modify aula025 and 029

--- aula025_funcao_d_seta.js
+++ aula025_funcao_d_seta.js
@@ -8,5 +8,5 @@ console.log(saudacao(meuNome))
 
 //outra vantagem desse tipo de função é que você não precisa colocar () se a função só tiver 1 argumento e nem {} se tiver apenas 1 expressão
 
-let promocao = nome=>`${nome}! Você foi promovido`
+let promocao = nome=>`${nome}! Você foi promovido`//observe que se você colocar chaves {} você obrigatoriamente precisa ter o return
 console.log(promocao(meuNome))
--- aula029_operador_this.js
+++ aula029_operador_this.js
@@ -1,15 +1,33 @@
 //o operador THIS indica que uma variável (this.variavel) pertence ao bloco de comando de onde ele está sendo executado e não de outra variável
 //de outro lugar, como uma variável VAR ou parâmetros de funções por exemplo.
 var nome = "João"
-var gmail = "kkk"
+var gmail = "Joao@gmail"
 
 function cliente(nome, gmail) {
     this.nome = nome
     this.gmail = gmail
 
-    function (nome){
-        
+    //Ao mesmo tempo que eu estou declarando uma função anônima eu estou dizendo que ela só é válida nesse escopo
+    this.mensagem1 = (nome, gmail)=>
+        `Olá ${nome}! seu gmail ${gmail} foi cadastrado com sucesso`
+    
+    //Nesse caso eu não preciso inserir as variáveis pois ele já consegue pega-las do escopo raiz (function cliente)
+    this.mensagem2 = ()=>{
+        return `Olá ${this.nome}! seu gmail ${this.gmail} foi cadastrado com sucesso`
     }
 
-    return 
+    //eu não posso colocar o "this" em uma função "normal"
+    function mensagem3 () {
+        return `Olá ${this.nome}! seu gmail ${this.gmail} foi cadastrado com sucesso`
+    }
+
+    let mensagens = [this.mensagem1(nome, gmail), this.mensagem2(), mensagem3()]
+
+    return mensagens
 }
+
+for(mensagem of cliente("Ronaldo","Ro@gmail")) {
+    console.log(mensagem)
+}
+
+//código imcompleto (não em função, mas em relação à aula)
