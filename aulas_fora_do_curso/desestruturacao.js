3f0f83b1abb9a5f3a36542aa5875589fe495145e
Paulo-V-Developer34 <paulov2007nv@gmail.com>
2024-09-30 12:32:49 -0300

    init desestruturacao.js

---
+++ aulas_fora_do_curso/desestruturacao.js
@@ -0,0 +1,7 @@
+//podemos pegar apenas dados específicos sobre outros dados (objetos, arrays, variáveis)
+
+//arrays
+
+const animais = ['gato', 'cachorro', 'coelho', 'sapo']
+
+const { animalfavorito } = animais
