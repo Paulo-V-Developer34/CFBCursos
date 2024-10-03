2f921584877f84aecb958ed9f06e4a795fe9a9f4
Paulo-V-Developer34 <paulov2007nv@gmail.com>
2024-10-03 07:43:13 -0300

    desestruturing system

--- aulas_fora_do_curso/desestruturacao.js
+++ aulas_fora_do_curso/desestruturacao.js
@@ -4,4 +4,35 @@
 
 const animais = ['gato', 'cachorro', 'coelho', 'sapo']
 
-const { animalfavorito } = animais
+// const { animalfavorito } = animais //com {} vai dar errado, devemos utilizar []
+const [ animalfavorito ] = animais //vai pegar o primeiro elemento do array
+
+const [ animal1, animal2 ] = animais //vai pegar os 2 primeiros elementos do array e assim em diante
+
+console.log(`${animalfavorito}, an1 = ${animal1} an2 = ${animal2}`)
+
+//objetos
+const pessoa = {
+    nome: "Paulo",
+    telefone1: 23234,
+    endereco: {
+        numero: 123,
+        bairro: "bairro PV",
+        rua: "rua do P"
+    },
+    filhos: [
+        'Sales',
+        'Rai',
+        'Jhonata'
+    ]
+}
+
+const {nome} = pessoa //extrai apenas 1 atributo //também posso extrair funções :D
+
+const {telefone1: tel1} = pessoa //extrai um valor e renomeia ele
+
+const {endereco: {numero: num}} = pessoa
+
+console.log(`Meu nome é ${nome}, meu telefone é ${tel1}. \nO número da minha casa é ${num}`)
+
+
